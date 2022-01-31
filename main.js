1a71356d8460d0e1d8bc5d6b4d99349a5c21271c
Demetrio Turchetta <Kensir96@gmail.com>
2022-01-31 15:59:02 +0100

    Push

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const gridElement = document.getElementById('grid');
 
-for (let i = 1; i<=64; i++) {
+const play = document.getElementById('bottone')
 
-    const node = document.createElement('div');
-    node.classList.add('square');
 
-    node.addEventListener('click', function(){
-        console.log(this);
-        this.classList.add('clicked');
-    })
+bottone.addEventListener('click',
+    function() {
+        for (let i = 1; i<=64; i++) {
 
-    gridElement.appendChild(node);
+            const node = document.createElement('div');
+            node.classList.add('square');
+        
+            node.addEventListener('click', function(){
+                console.log(this);
+                this.classList.add('clicked');
+            })
+        
+            gridElement.appendChild(node);
+        
+        }
+    }
+);
 
-}https://github.com/Kensir01/js-campominato-grid.git
